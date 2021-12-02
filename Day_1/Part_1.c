762f77dd82d9f8630a29e42280fcc2d455415911
Luca Borrello <81256628+ilmangusta@users.noreply.github.com>
2021-12-02 13:30:19 +0100

    Update Part_1.c

--- Day_1/Part_1.c
+++ Day_1/Part_1.c
@@ -8,22 +8,18 @@ int main(void) {
   file1=fopen("imput.txt", "r");
   char row[N]="";
   int deep[N];
-  int val;
   int i=0;
   while(1){
     if (feof(file1)){
       break;
     }
     fgets(row, N, file1);
-    val=atoi(row);
-    deep[i]=val;
+    deep[i]=atoi(row);
     i++;
     strcpy(row," ");
-
   }
   int count=0;
   for (int j=0; j<i; j++){
-    printf("deep[j]: %d\n",deep[j]);
     if (deep[j]==2682){
       break;
     }
