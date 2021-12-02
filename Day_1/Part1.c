5b4160b9ea9850a4d5afdcd513e4955983cd0554
Luca Borrello <81256628+ilmangusta@users.noreply.github.com>
2021-12-02 15:24:32 +0100

    Update Part1.c

--- Day_1/Part1.c
+++ Day_1/Part1.c
@@ -4,29 +4,23 @@
 #include <stdlib.h>
 
 int main(void) {
-  FILE* file1;
-  file1=fopen("imput.txt", "r");
+  FILE* file1=fopen("imput.txt", "r");
   char row[N]="";
-  int deep[N];
-  int i=0;
-  while(1){
-    if (feof(file1)){
-      break;
-    }
+  int deep[N], i=0;
+  
+  while(!feof(file1)){
     fgets(row, N, file1);
     deep[i]=atoi(row);
     i++;
     strcpy(row," ");
   }
+  
   int count=0;
-  for (int j=0; j<i; j++){
-    if (deep[j]==2682){
-      break;
-    }
+  for (int j=0; j<i-1; j++){
     if (deep[j]<deep[j+1]){
       count=count+1;
     }
   }
-  printf("count: %d", count);
+  printf("count: %d", count); //Result
   return 0;
 }
